2c0a8bf2ae086b70dda81d1781e112bdac4656fa
SujoyDevnath <sujoydevnathdipta@gmail.com>
2021-11-13 18:03:04 +0600

    k

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// name: taarzan
-// password: FmmotReMFBKByPee
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s8i5p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
